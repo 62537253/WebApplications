9b234255c8cc5e6638a9e56adc039e7a2b363838
62537253 <sakuraogonkova@gmail.com>
2023-10-30 16:45:01 +0700

    First application

---
+++ src/main/java/kuzstu/ru/WebApplicationSchool/WebApplicationSchoolApplication.java
@@ -0,0 +1,13 @@
+package kuzstu.ru.WebApplicationSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kuzstu/ru/WebApplicationSchool/WebApplicationSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package kuzstu.ru.WebApplicationSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
